3057faab48d6e3c7478b06d4f453f2aa9ad70fb5
Stephen Klein <stephendklein08@gmail.com>
2020-03-26 23:23:38 -0700

    GET bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,17 +5,33 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all boot camps'});
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps});
+    } catch (err) {
+        res.status(400).json({success: false});
+    }
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Show boot camp ${req.params.id}`});
-}
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        //if their is no data for req id
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+        // this return an success: false incase their is not data matching the object id
+        res.status(200).json({ success: true, data: bootcamp});
 
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+}
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
