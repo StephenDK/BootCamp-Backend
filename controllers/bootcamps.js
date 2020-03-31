e40238336c7cfef2a1b5cf0cb55a4a951971f19e
Stephen Klein <stephendklein08@gmail.com>
2020-03-31 13:53:16 -0700

    Error Handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,8 +9,9 @@ exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps});
-    } catch (err) {
-        res.status(400).json({success: false});
+    } catch (error) {
+        //res.status(400).json({success: false});
+        next(error);
     }
 }
 
@@ -32,7 +33,7 @@ exports.getBootcamp = async (req, res, next) => {
 
     } catch (error) {
         //res.status(400).json({ success: false });
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        next(error);
     }
 }
 // @desc    Create new bootcamp
@@ -49,7 +50,8 @@ exports.createBootcamp = async (req, res, next) => {
         })
         
     } catch (error) {
-        res.status(400).json({ success: false})
+        //res.status(400).json({success: false });
+        next(error);
     }
 
 }
@@ -65,12 +67,15 @@ exports.updateBootcamp = async (req, res, next) => {
         });
         // make sure bootcamps exists
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
         }
         res.status(200).json({success: true, data: bootcamp});
         
     } catch (error) {
-        res.status(400).json({success: false });
+        //res.status(400).json({success: false });
+        next(error);
     }
 }
 
@@ -82,12 +87,15 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
         }
 
         res.status(200).json({ success: true, data: {} })
     } catch (error) {
-        res.status(400).json({success: false });
+        //res.status(400).json({success: false });
+        next(error);
     }
 
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,22 @@
-const errorHandler = (err, req, res, next) => {
-    //log to console for developer
-    console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
 
-    res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) => {
+    let error = { ...err };
+
+    error.message = err.message;
+    //log to console for developer
+    // console.log(err.stack.red);
+
+    // Mongoose bad objectId
+    //console.log(err.name);
+    if (err.name === 'CastError') {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     });
 }
 
