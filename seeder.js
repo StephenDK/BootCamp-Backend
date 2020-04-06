5a1a7e2be3a0b9ff877257d30506efcf14904f3f
Stephen Klein <stephendklein08@gmail.com>
2020-04-05 17:05:52 -0700

    Database Seeder

---
+++ seeder.js
@@ -0,0 +1,62 @@
+// This file seeds our database with data
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+// console.log(bootcamps);
+
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data Imported....'.green.inverse);
+        //exit process
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data Deleted....'.red.inverse);
+        //exit process
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    // node seeder -i (seed database)
+    importData();
+} else if (process.argv[2] === '-d') {
+    // node seeder -d (delete data)
+    deleteData();
+}
