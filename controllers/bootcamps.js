59f38b3f9aad0ddbd008d436a6118fbfc6fcce83
Stephen Klein <stephendklein08@gmail.com>
2020-08-30 18:08:28 -0700

    Selecting and Sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,20 +10,72 @@ const geocoder = require('../utils/geocoder');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     // Getting query params
     // Example of a query params /api/v1/bootcamps/?location.state=MA&housing=true
-    console.log(req.query);
+    // console.log(req.query);
 
-    // Query mongodb using greater then and less then
+    // Setting up selecting and sorting
+    // copy req.query
+    const reqQuery = {...req.query}
+    // console.log('18. reqQuery ', reqQuery)
+    
+    // Fields to exclude
+    const removeFields = ['select', 'sort']
+    // console.log(removeFields);
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param]);
+    // console.log(removeFields);
+
+    // // Query mongodb using greater then and less then
     let query;
 
-    let queryStr = JSON.stringify(req.query);
-
+    // Take the query object and stringify it.
+    let queryStr = JSON.stringify(reqQuery);
+    // console.log('32. QUERY STRING BEFORE: ',typeof(queryStr), queryStr);
+    // console.log(queryStr);
+    // Take the string and replace a $ before the query selector
+ 
+    // Name	Description
+    // $eq	Matches values that are equal to a specified value.
+    // $gt	Matches values that are greater than a specified value.
+    // $gte	Matches values that are greater than or equal to a specified value.
+    // $in	Matches any of the values specified in an array.
+    // $lt	Matches values that are less than a specified value.
+    // $lte	Matches values that are less than or equal to a specified value.
+    // $ne	Matches all values that are not equal to a specified value.
+    // $nin	Matches none of the values specified in an array.
+   
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    console.log(queryStr);
+    // console.log('47. QUERY STRING: AFTER', typeof(queryStr), queryStr);
+    
 
+    // console.log('51. QUERYSTR: ', JSON.parse(queryStr));
     query = Bootcamp.find(JSON.parse(queryStr));
+    // console.log(Object.keys(query));
+
+    // console.log('req.query.select: ', req.query.select);
+    //Select Felds
+    if (req.query.select) {
+        // URL selection example
+        // /api/v1/bootcamps?select=name,description,housing
+        const fields = req.query.select.split(',').join(' ');
+        // console.log('57. fields: ',fields);
+        // console.log(query);
+        query = query.select(fields);
+        // console.log(query);
+    }
+
+    // Sort
+    if (req.query.sort) {
+        // Sort URL example
+        // /api/v1/bootcamps?select=name,description,housing&sort=name
+        // for decending put - in front of the sort=-name
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
 
     const bootcamps = await query;
-
+    console.log(bootcamps)
     res
         .status(200)
         .json({ success: true, count: bootcamps.length, data: bootcamps});
