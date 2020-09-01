1012ff638d319e8e069cafe905d77be0c379ba2b
Stephen Klein <stephendklein08@gmail.com>
2020-08-31 21:54:46 -0700

    Course Model and Seeding

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,12 +27,15 @@ const bootcamps = JSON.parse(
 );
 // console.log(bootcamps);
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-
+        await Course.create(courses)
         console.log('Data Imported....'.green.inverse);
         //exit process
         process.exit();
@@ -44,7 +48,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-
+        await Course.deleteMany()
         console.log('Data Deleted....'.red.inverse);
         //exit process
         process.exit();
