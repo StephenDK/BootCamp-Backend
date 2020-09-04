e04bb3dec57fe86dc23ab8664f703603e0de41d1
StephenDK <stephendklein08@gmail.com>
2020-09-03 21:45:02 -0700

    Aggregate Average Cost

--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,49 @@ const CourseSchema = new mongoose.Schema({
     }
 });
 
+// statics vs methods
+// we are defining a static like so.
+// a static is accessible like so Course.getAverageCost
+// a method is like so
+// const query = await Course.find()
+// query.getAverageCost => methods are found on the query object
+
+// static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+    console.log('Calculating average cost...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: {$avg: '$tuition'}
+            }
+        }
+    ])
+    console.log(obj);
+    // we are getting the averageCost from obj above
+    // to save it into the db
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// Call getAverageCost after save
+CourseSchema.post('save', function() {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function() {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+
 module.exports = mongoose.model('Course', CourseSchema);
