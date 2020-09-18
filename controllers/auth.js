10b865f344fbeefe4bc263c85ff16bb477fe6395
Stephen Klein <stephendklein08@gmail.com>
2020-09-17 18:33:38 -0700

    Forgot Password - Generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,45 @@ exports.login = asyncHandler(async (req, res, next) => {
 })
 
 
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+
+// @desc    Forgot Password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.ForgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next( new ErrorResponse('There is no user with that email', 404));
+    };
+
+    //  Get reset Token
+    const resetToken = user.getResetPasswordToken();
+    // console.log(resetToken);
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+
+
+
+
 
 // Custom function to get token from model and create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
@@ -81,15 +120,3 @@ const sendTokenResponse = (user, statusCode, res) => {
         });
 
 }
-
-// @desc    Get current logged in user
-// @route   GET /api/v1/auth/me
-// @access  private
-exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: user
-    })
-})
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { register, login, getMe } = require('../controllers/auth');
+const { register, login, getMe, ForgotPassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', ForgotPassword);
+
 
 
 
