ffcc9c6cdc558549ab24b832bf2b811d6d02acfc
StephenDK <stephendklein08@gmail.com>
2020-09-21 22:35:24 -0700

    Aggregate - Calculate Average Rating

--- models/Review.js
+++ models/Review.js
@@ -39,4 +39,42 @@ const ReviewSchema = new mongoose.Schema({
 // This prevents users from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1}, { unique: true })
 
+
+// static method to get average rating
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    // console.log('Calculating average cost...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: {$avg: '$rating'}
+            }
+        }
+    ])
+    console.log(obj);
+    // we are getting the averageCost from obj above
+    // to save it into the db
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// Call getAverageCost after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
+// Call getAverageCost before remove
+ReviewSchema.pre('remove', function() {
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
 module.exports = mongoose.model('Review', ReviewSchema);
